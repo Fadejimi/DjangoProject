347d6d9dfd456997c938e63a7463df746014fab5
Fadejimi <adegbulugbefadejimi@gmail.com>
2015-06-23 22:38:23 +0100

    Updated templates Feeling super cool

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
-urlpatters = [
+urlpatterns = [
     url(r'^$', views.index, name='index'),
     # ex: /polls/5/
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+#from django.template import loader, RequestContext
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You are at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
-def details(request, question_id):
-    return HttpResponse("You are looking at quesiton %s." % question_id)
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question':question})
 
 def results(request, question_id):
     response = "You are looking at the results of the question %s."
