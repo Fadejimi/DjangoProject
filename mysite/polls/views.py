b6126aaa4ae091e499be27235b668e7e06e466db
Fadejimi <adegbulugbefadejimi@gmail.com>
2015-06-23 18:57:21 +0100

    Getting accomsted to the views of Django Loving this framework

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world. You are at the polls index.")
+
+def details(request, question_id):
+    return HttpResponse("You are looking at quesiton %s." % question_id)
+
+def results(request, question_id):
+    response = "You are looking at the results of the question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
